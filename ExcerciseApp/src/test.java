1a55a3184a4a368d76b20de4c0d542fe0057d27f
Andrew Cao <andrewjcao@gmail.com>
2020-06-18 15:25:35 -0400

    Made test cases more readable

--- ExcerciseApp/src/test.java
+++ ExcerciseApp/src/test.java
@@ -4,25 +4,31 @@ public class test
 	//Simple Test Cases
 	public static void main(String[] args)
 	{
+		//Creates excercise
 		ExcerciseClass a = new ExcerciseClass("Pull-Ups", "Pull Yourself Up", MuscleGroup.BACK);
 		a.addSuppMuscle(MuscleGroup.BICEPS);
 		a.addSuppMuscle(MuscleGroup.SHOULDERS);
 		a.addEquipment("Bar");
 		
+		//Creates excercise
 		ExcerciseClass b = new ExcerciseClass("Push Up", "Push off the ground", MuscleGroup.CHEST);
 		b.addSuppMuscle(MuscleGroup.BICEPS);
 		b.addSuppMuscle(MuscleGroup.TRICEPS);
 		
+		//Creates excercise
 		ExcerciseClass c = new ExcerciseClass("Barbell Bench", "Push a heavy Stick", MuscleGroup.CHEST);
 		c.addSuppMuscle(MuscleGroup.BICEPS);
 		c.addSuppMuscle(MuscleGroup.SHOULDERS);
 		c.addEquipment("Barbell");
 		c.addEquipment("Bench Press");
 		
+		//Creates table and adds excercise to table
 		ExcerciseTable table = new ExcerciseTable();
 		table.addExcercise(a);
 		table.addExcercise(b);
 		table.addExcercise(c);
+		
+		//Prints out excercises in table
 		System.out.println(table.toString());
 
 	}
